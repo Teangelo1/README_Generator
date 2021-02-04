5e277a80e0ce1b2ec0ba88d720a6c8a3935b1c30
Teangelo Burks <burks.teangelo@gmail.com>
2021-02-04 18:28:17 -0500

    prompt for asking questions to generate readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,49 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require('./utils/generateMarkdown')
+
 
 // TODO: Create an array of questions for user input
-const questions = [
 
-    
-];
+     inquirer.prompt (
+
+        [
+
+            {
+                type: "input",
+                name: "title",
+                message: "What is the title of your project?"
+
+
+            },
+
+            {
+                type: "input",
+                name: "description",
+                message: "Provide a description of your project?"
+
+
+            },
+
+            {
+                type: "input",
+                name: "installation",
+                message: "What steps are required to install your project?"
+
+
+            }
+
+
+
+        ]
+    )
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
